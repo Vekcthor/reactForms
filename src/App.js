26ded2f77ef156aaeb0fa85e184f4515dabcefca
Victor Azevedo <victor.sil.azevedo@gmail.com>
2021-08-04 19:37:46 -0300

    Versão Final, com validação e submit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
 import '@fontsource/roboto';
-import { Container , Typography} from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Component } from 'react';
 
 
-function App() {
-  return (
-    <Container component="article" maxWidth="sm">
-      <Typography variant="h3" aling="center" component="h1">Formulario de Cadastro</Typography>
-      <FormularioCadastro />
-    </Container>
+class App extends Component {
+  render() {
+    return (
+      <Container component="article" maxWidth="sm">
+        <Typography variant="h3" aling="center" component="h1">Formulario de Cadastro</Typography>
+        <FormularioCadastro onSubmit={submitForm} validarCPF={validarCPF} />
+      </Container>
+    );
+  }
+}
 
-  );
+function submitForm(dados){
+  console.log(dados);
+}
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 dígitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
 }
 
 export default App;
--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,33 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Switch, FormControlLabel } from '@material-ui/core';
 
-function FormularioCadastro() {
-    return (
-        <form>
-            <TextField id="nome" label="Nome" margin="normal" variant="outlined" fullWidth/>
-            
-            <TextField id="sobrenome" label="Sobrenome" margin="normal" variant="outlined" fullWidth />
+function FormularioCadastro({onSubmit,validarCPF}) {
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [promo, setPromo] = useState(true);
+    const [novidades, setNovidades] = useState(true);
+    const [erros, setErros] = useState({cpf:{valido:true, texto:""}});
 
-            <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
+    return (
+        <form onSubmit={(event) => { event.preventDefault(); onSubmit({nome,sobrenome,cpf,promo,novidades}); }} >
+            <TextField value={nome} onChange={(event)=> {setNome(event.target.value); } } id="nome" label="Nome" margin="normal" variant="outlined" fullWidth />
+
+            <TextField value={sobrenome} onChange={(event)=> {setSobrenome(event.target.value); } } id="sobrenome" label="Sobrenome" margin="normal" variant="outlined" fullWidth />
+
+            <TextField 
+                value={cpf} 
+                onChange={(event)=> {setCpf(event.target.value); } } 
+                error = {!erros.cpf.valido}
+                helperText = {erros.cpf.texto}
+                onBlur={(event)=>{
+                    const ehValido = validarCPF(cpf)
+                    setErros({cpf:ehValido})}}
+                id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth
+                type="number" 
+            />
+            
             <FormControlLabel
-        control={
-            <Switch
-        color="primary"
-        name="checkedB"
-        inputProps={{ 'aria-label': 'Promocoes' }}
-        defaultChecked
-      /> }
-      label="Promoções"/>
-<FormControlLabel
-        control={
-<Switch
-        color="primary"
-        name="checked"
-        inputProps={{ 'aria-label': 'Noviidades' }}
-        defaultChecked
-      />
-        }
-        label="Noviidades"/>
+                control={
+                    <Switch
+                        checked={promo}
+                        color="primary"
+                        name="checkedB"
+                        inputProps={{ 'aria-label': 'Promocoes' }}
+                        onChange={(event)=> {setPromo(event.target.checked); } }
+                    />}
+                label="Promoções" />
+            
+            <FormControlLabel
+                control={
+                    <Switch
+                        checked={novidades}
+                        color="primary"
+                        name="checked"
+                        inputProps={{ 'aria-label': 'Novidades' }}
+                        onChange={(event)=> {setNovidades(event.target.checked); } }
+                    />
+                }
+                label="Novidades" />
 
             <Button variant="contained" color="primary" type="submit" >Cadastrar</Button>
         </form>)
